c6f9b6a1877fccc5612b4f91a50f78f39d992073
rasul-surname <rasul.surname@mail.ru>
2023-03-07 11:15:14 +0300

    Added react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,35 @@ import type { AppProps } from 'next/app'
 import { ChakraBaseProvider } from '@chakra-ui/react'
 import theme from '@/theme'
 import Head from 'next/head'
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App({ Component, pageProps }: AppProps) {
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						refetchOnWindowFocus: false,
+					},
+				},
+			})
+	)
+
 	return (
-		<ChakraBaseProvider theme={theme}>
-			<Head>
-				<title>Test json-server</title>
-			</Head>
-			<Component {...pageProps} />
-		</ChakraBaseProvider>
+		<>
+			<QueryClientProvider client={queryClient}>
+				<ReactQueryDevtools initialIsOpen={false} />
+
+				<ChakraBaseProvider theme={theme}>
+					<Head>
+						<title>Test json-server</title>
+					</Head>
+					<Component {...pageProps} />
+				</ChakraBaseProvider>
+			</QueryClientProvider>
+		</>
 	)
 }
 
